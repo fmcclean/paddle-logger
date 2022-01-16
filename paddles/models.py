27cfebaf9dfe62b5887637515d981bb5c9abf8f2
Fergus McClean <fergus.mcclean@icloud.com>
2022-01-16 09:59:42 +0000

    Add models

--- paddles/models.py
+++ paddles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class River(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    url = models.URLField()
+
+
+class Paddle(models.Model):
+    id = models.AutoField(primary_key=True)
+    river = models.ForeignKey(River, on_delete=models.SET_NULL, null=True)
+    level = models.FloatField()
+    date = models.DateField()
+    description = models.TextField()
